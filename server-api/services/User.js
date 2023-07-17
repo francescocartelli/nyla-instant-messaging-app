37bb8713bb430122b8d71f7325c236f1f136f634
francescocartelli <60184893+francescocartelli@users.noreply.github.com>
2023-07-17 08:55:31 +0200

    Fixed pattern in user services

--- server-api/services/User.js
+++ server-api/services/User.js
@@ -4,18 +4,18 @@ const mongo = require.main.require('./components/db')
 
 const db = mongo.get()
 
-const userProjection = { projection: { _id: 0, id: '$_id', username: 1, email: 1, confirmed: 1 } }
+const userProjection = { _id: 0, id: '$_id', username: 1, email: 1, confirmed: 1 }
 
 exports.getUsers = (username) => {
     let query = { username: { $regex: username, $options: 'i' } }
-    return db.collection(db.collections.user).find(query, userProjection).limit(db.configs.USERS_PER_PAGE).toArray()
+    return db.collection(db.collections.user).find(query, { projection: userProjection }).limit(db.configs.USERS_PER_PAGE).toArray()
 }
 
 exports.getUser = (user = {}) => {
     let { id, ...u } = user
     if (id) u._id = new ObjectId(id)
 
-    return db.collection(db.collections.user).findOne(u, userProjection)
+    return db.collection(db.collections.user).findOne(u, { projection: userProjection })
 }
 
 exports.getUserId = (id) => {
@@ -40,4 +40,8 @@ exports.updateUser = (id, user) => {
 /* semicit: user cannot be deleted, they simply result missing */
 exports.deleteUser = (id) => {
     return db.collection(db.collections.user).deleteOne({ _id: new ObjectId(id) })
+}
+
+exports.getFullUsers = (ids) => {
+    return db.collection(db.collections.user).find({ _id: { $in: ids.map(i => new ObjectId(i)) } }, { projection: userProjection }).toArray()
 }
