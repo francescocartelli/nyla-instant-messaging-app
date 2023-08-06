4bbb28be7775ca7e5fc4e1261e6c23e233a1e953
francescocartelli <60184893+francescocartelli@users.noreply.github.com>
2023-08-06 08:08:42 +0200

    Added Swagger Open-API to api server Fixed npm modules vulnerailities

--- server-api/server.js
+++ server-api/server.js
@@ -25,6 +25,13 @@ db.connect(() => {
 })
 
 function boot() {
+  /* Swagger */
+  const swaggerUI = require('swagger-ui-express')
+  const swaggerDocument = require('./api-docs.json')
+
+  // swagger documentation
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
   /* Initialize passport */
   const passport = require('passport')
   app.use(passport.initialize())
