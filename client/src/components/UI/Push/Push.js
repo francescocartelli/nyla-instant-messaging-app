d1ab6cfd9c8191910ca62bcf219df31fcf35968c
francescocartelli <60184893+francescocartelli@users.noreply.github.com>
2024-01-12 23:40:49 +0100

    Updated push notification behavior

--- client/src/components/UI/Push/Push.js
+++ client/src/components/UI/Push/Push.js
@@ -1,29 +1,32 @@
 import { useContext, useEffect, useRef, useState } from 'react'
 import { InfoCircle, XCircle } from 'react-bootstrap-icons'
+import { useNavigate } from 'react-router-dom'
 
 import './Push.css'
 import 'styles/style.css'
 
 import { WebSocketContext, channelTypes } from 'components/Ws/WsContext'
 
-function Notification({ notification, onClose, delay = 4000 }) {
+function Notification({ title, text, onClick, onClose, delay = 4000 }) {
     const refOnClose = useRef(onClose)
     useEffect(() => {
         const timeout = setTimeout(() => refOnClose.current(), delay)
         return () => clearTimeout(timeout)
     }, [delay])
 
-    return <div className='d-flex flex-row align-items-center card-1 gap-2 box-glow'>
+    return <div className='d-flex flex-row align-items-center card-1 gap-2 box-glow' onClick={() => { onClick(); onClose() }}>
         <InfoCircle className='fore-2 size-2 flex-shrink-0' />
         <div className='d-flex flex-column flex-grow-1' style={{ minWidth: 0 }}>
-            <p className='crd-subtitle text-truncate'>{notification.title}</p>
-            <p className='m-0 text-truncate'>{notification.text}</p>
+            <p className='crd-subtitle text-truncate'>{title}</p>
+            <p className='m-0 text-truncate'>{text}</p>
         </div>
-        <XCircle className='fore-2-btn size-1 flex-shrink-0' onClick={() => onClose()} />
+        <XCircle className='fore-2-btn size-1 flex-shrink-0' onClick={(ev) => { ev.stopPropagation(); onClose() }} />
     </div>
 }
 
 function PushContainer({ maxNoticationsN = 4 }) {
+    const navigate = useNavigate()
+
     const [notifications, setNotifications] = useState([])
     const [subscribe, unsubscribe] = useContext(WebSocketContext)
 
@@ -34,23 +37,21 @@ function PushContainer({ maxNoticationsN = 4 }) {
         const addNotification = (notification) => setNotifications(p => {
             return p.length >= maxNoticationsN ? [...p.slice(1), notification] : [...p, notification]
         })
-        
+
         const channelDefault = channelTypes.createMessage()
         subscribe(channelDefault, ({ message }) => {
             addNotification({
                 id: message.id,
-                title: message.chatName ? `Message from ${message.senderUsername} in ${message.chatName}` : `Message from ${message.senderUsername}`,
+                onClick: () => { navigate(`/chats/${message.idChat}`) },
+                title: message.chatName ? <>Message from <b>{message.senderUsername}</b> in <b>{message.chatName}</b></> : <>Message from <b>{message.senderUsername}</b></>,
                 text: message.content
             })
         })
         return () => { unsubscribe(channelDefault) }
-    }, [maxNoticationsN, subscribe, unsubscribe])
+    }, [maxNoticationsN, subscribe, unsubscribe, navigate])
 
     return <div className='push-wrapper adaptive-p'>
-        {notifications?.map(n => {
-            return <Notification key={n.id} notification={n}
-                onClose={() => removeNotication(n)} />
-        })}
+        {notifications?.map(({ id, ...n }) => <Notification key={id} {...n} onClose={() => removeNotication({ id })} />)}
     </div>
 }
 
