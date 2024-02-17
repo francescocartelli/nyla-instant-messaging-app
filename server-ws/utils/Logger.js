46d8be3852984f41c071e6be9b1c0dc73bb5a18b
francescocartelli <60184893+francescocartelli@users.noreply.github.com>
2024-02-17 19:13:35 +0100

    Added logging level in server-ws

--- server-ws/utils/Logger.js
+++ server-ws/utils/Logger.js
@@ -1,7 +1,9 @@
 const winston = require('winston')
 
+require('dotenv').config()
+
 exports.logger = winston.createLogger({
-    level: 'info',
+    level: process.env.LOGGING_LEVEL,
     format: winston.format.combine(
         winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
         winston.format.printf(({ timestamp, message }) => `${timestamp} | ${message}`)
